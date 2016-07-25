e4c523f894f64032c217e1fa8238d5bd167d85e3
Chris Wilson <chris2097@gmail.com>
2016-07-25 17:44:59 +0100

    working on making buttons flash

--- js/fcc_simon_game.js
+++ js/fcc_simon_game.js
@@ -4,7 +4,7 @@ var sequence = [],
     redButton = document.getElementById("red"),
     greenButton = document.getElementById("green"),
     yellowButton = document.getElementById("yellow"),
-    count = 1;
+    count = 5;
 
 // start the game
 // populate sequence
@@ -28,9 +28,50 @@ function toggleStrict(){
 
 // display the sequence and play sounds
 function flashSequence(){
+    console.log("flash triggered");
+var delay;
+    if (count < 6){
+        delay = 420;
+    }
+    else if (count < 14){
+        delay = 320;
+    }
+    else{
+        delay = 220;
+    }
+    console.log(delay);
+for (var i = 0; i <= count; i++){
+    console.log("for triggered");
+    console.log(sequence[i]);
+    switch(sequence[i]){
+        case "blue":
+            console.log("blue triggered");
+            blueButton.className = "quarter blueFlash";
+            new Audio('https://s3.amazonaws.com/freecodecamp/simonSound1.mp3').play();
+            setTimeout(function(){
+                blueButton.className = "quarter";
+            break;
+            }, delay);
 
-for (var i= 0; i<count; i++){
-
+        case "red":
+            console.log("red triggered");
+            redButton.className = "quarter redFlash";
+            new Audio('https://s3.amazonaws.com/freecodecamp/simonSound2.mp3').play();
+            setTimeout(function(){redButton.className = "quarter"}, delay);
+            break;
+        case "yellow":
+            console.log("yellow triggered");
+            yellowButton.className = "quarter yellowFlash";
+            new Audio('https://s3.amazonaws.com/freecodecamp/simonSound3.mp3').play();
+            setTimeout(function(){yellowButton.className = "quarter"}, delay);
+            break;
+        case "green":
+            console.log("green triggered");
+            greenButton.className = "quarter greenFlash";
+            new Audio('https://s3.amazonaws.com/freecodecamp/simonSound4.mp3').play();
+            setTimeout(function(){greenButton.className = "quarter"}, delay);
+            break;
+    }
 }
 }
 
