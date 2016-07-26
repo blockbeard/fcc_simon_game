5e77c6e287faf67714747628bca53940a785abd3
Chris Wilson <chris2097@gmail.com>
2016-07-26 10:50:44 +0100

    buttons now flashing, timing is a PITA

--- js/fcc_simon_game.js
+++ js/fcc_simon_game.js
@@ -40,39 +40,54 @@ var delay;
         delay = 220;
     }
     console.log(delay);
-for (var i = 0; i <= count; i++){
-    console.log("for triggered");
-    console.log(sequence[i]);
-    switch(sequence[i]){
-        case "blue":
-            console.log("blue triggered");
-            blueButton.className = "quarter blueFlash";
-            new Audio('https://s3.amazonaws.com/freecodecamp/simonSound1.mp3').play();
-            setTimeout(function(){
-                blueButton.className = "quarter";
-            break;
-            }, delay);
+            for (var i = 0; i <= count; i++) {
+                setDelay(i)
+            }
+function setDelay(i){
+    setTimeout(function() {
+                console.log("for triggered");
+                console.log(sequence[i]);
 
-        case "red":
-            console.log("red triggered");
-            redButton.className = "quarter redFlash";
-            new Audio('https://s3.amazonaws.com/freecodecamp/simonSound2.mp3').play();
-            setTimeout(function(){redButton.className = "quarter"}, delay);
-            break;
-        case "yellow":
-            console.log("yellow triggered");
-            yellowButton.className = "quarter yellowFlash";
-            new Audio('https://s3.amazonaws.com/freecodecamp/simonSound3.mp3').play();
-            setTimeout(function(){yellowButton.className = "quarter"}, delay);
-            break;
-        case "green":
-            console.log("green triggered");
-            greenButton.className = "quarter greenFlash";
-            new Audio('https://s3.amazonaws.com/freecodecamp/simonSound4.mp3').play();
-            setTimeout(function(){greenButton.className = "quarter"}, delay);
-            break;
-    }
-}
+
+                    blueButton.style.backgroundColor = "";
+                    redButton.style.backgroundColor = "";
+                    yellowButton.style.backgroundColor = "";
+                    greenButton.style.backgroundColor = "";
+
+                    switch (sequence[i]) {
+                        case "blue":
+                            console.log("blue triggered");
+                            blueButton.style.backgroundColor = "lightblue";
+                            new Audio('https://s3.amazonaws.com/freecodecamp/simonSound1.mp3').play();
+                            break;
+                        case "red":
+                            console.log("red triggered");
+                            redButton.style.backgroundColor = "lightcoral";
+                            new Audio('https://s3.amazonaws.com/freecodecamp/simonSound2.mp3').play();
+                            break;
+                        case "yellow":
+                            console.log("yellow triggered");
+                            yellowButton.style.backgroundColor = "lightyellow";
+                            new Audio('https://s3.amazonaws.com/freecodecamp/simonSound3.mp3').play();
+                            break;
+                        case "green":
+                            console.log("green triggered");
+                            greenButton.style.backgroundColor = "lightgreen";
+                            new Audio('https://s3.amazonaws.com/freecodecamp/simonSound4.mp3').play();
+                            break;
+                        default:
+                            console.log("Switch Error");
+                    }
+
+
+                }, delay*i+1);
+            }
+            setTimeout(function(){
+                blueButton.style.backgroundColor = "";
+                redButton.style.backgroundColor = "";
+                yellowButton.style.backgroundColor = "";
+                greenButton.style.backgroundColor = "";
+            }, delay);
 }
 
 // take the player input
